13b0101f8404cb8819437be655d8f52568ddfcbd
Stephyyy D <stephyyyd.@Stephyyys-MacBook-Air.local>
2019-02-07 23:20:25 -0500

    updated css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  
+<div className="container">
+  <div className="calc-body">
+      
+      <div className="calc-input scroll">0000000000000000</div>
+     
+    
+    <div className="calc-button-row">
+      <div className="button ops">AC</div>
+      <div className="button ops">%</div>
+      <div className="button ops">±</div>
+      <div className="button ops">÷</div>
+    </div>
+    <div className="calc-button-row">
+      <div className="button">7</div>
+      <div className="button">8</div>
+      <div className="button">9</div>
+      <div className="button ops">x</div>
+    </div>
+    <div className="calc-button-row">
+      <div className="button">4</div>
+      <div className="button">5</div>
+      <div className="button">6</div>
+      <div className="button ops">−</div>
+    </div>
+    <div className="calc-button-row">
+      <div className="button">1</div>
+      <div className="button">2</div>
+      <div className="button">3</div>
+      <div className="button ops">+</div>
+    </div>
+    <div className="calc-button-row">
+      <div className="double">0</div>
+      <div className="button">.</div>
+          <div className="button ops">=</div>
       </div>
-    );
+    </div>
+  </div>
+      );
   }
 }
 

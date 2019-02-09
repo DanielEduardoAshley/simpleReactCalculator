5afdde74ddebb635bad8960de413622b746a3547
Stephyyy D <stephyyyd.@Stephyyys-Air.fios-router.home>
2019-02-09 06:23:28 -0500

    deleted folders&updated calc body

--- src/App.js
+++ src/App.js
@@ -4,45 +4,43 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-  
-<div className="container">
-  <div className="calc-body">
-      
-      <div className="calc-input scroll">0000000000000000</div>
-     
-    
-    <div className="calc-button-row">
-      <div className="button ops">AC</div>
-      <div className="button ops">%</div>
-      <div className="button ops">±</div>
-      <div className="button ops">÷</div>
-    </div>
-    <div className="calc-button-row">
-      <div className="button">7</div>
-      <div className="button">8</div>
-      <div className="button">9</div>
-      <div className="button ops">x</div>
-    </div>
-    <div className="calc-button-row">
-      <div className="button">4</div>
-      <div className="button">5</div>
-      <div className="button">6</div>
-      <div className="button ops">−</div>
-    </div>
-    <div className="calc-button-row">
-      <div className="button">1</div>
-      <div className="button">2</div>
-      <div className="button">3</div>
-      <div className="button ops">+</div>
-    </div>
-    <div className="calc-button-row">
-      <div className="double">0</div>
-      <div className="button">.</div>
-          <div className="button ops">=</div>
+
+          <div className="container">
+          <div className="calc-body">
+          <div className="calc-input scroll">0000000000000000</div>
+
+          <div className="calc-button-row">
+            <div className="button ops">AC</div>
+            <div className="button ops">%</div>
+            <div className="button ops">±</div>
+            <div className="button ops">÷</div>
+          </div>
+          <div className="calc-button-row">
+            <div className="button">7</div>
+            <div className="button">8</div>
+            <div className="button">9</div>
+            <div className="button ops">x</div>
+          </div>
+          <div className="calc-button-row">
+            <div className="button">4</div>
+            <div className="button">5</div>
+            <div className="button">6</div>
+            <div className="button ops">−</div>
+          </div>
+          <div className="calc-button-row">
+            <div className="button">1</div>
+            <div className="button">2</div>
+            <div className="button">3</div>
+            <div className="button ops">+</div>
+          </div>
+          <div className="calc-button-row">
+            <div className="double">0</div>
+            <div className="button">.</div>
+            <div className="button ops">=</div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-      );
+    );
   }
 }
 
